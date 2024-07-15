76f9ea08b60890d5735350dbe244b8e92765dce5
William Grant <willmgrant@gmail.com>
2024-07-15 17:39:51 +1000

    re-add BufferClose since it doesn't impact window layouts, rather use close window keymap when we want to close a window

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -1,6 +1,6 @@
 return {
-  vim.keymap.set('n', 'gw', '<cmd>bdelete<cr>', { desc = 'Close the current buffer' }),
-  vim.keymap.set('n', 'gW', '<cmd>bdelete!<cr>', { desc = 'Close the current buffer (force)' }),
+  vim.keymap.set('n', 'gw', '<cmd>BufferClose<cr>', { desc = 'Close the current buffer' }),
+  vim.keymap.set('n', 'gW', '<cmd>BufferClose!<cr>', { desc = 'Close the current buffer (force)' }),
   vim.keymap.set('n', 'gt', '<cmd>terminal<cr>', { desc = 'Open terminal buffer' }),
   vim.keymap.set('n', 'gs', '<cmd>Telescope git_status<cr>', { desc = 'Show git status' }),
   vim.keymap.set('n', '<leader>wm', '<cmd>mksession<cr>', { desc = '[W]orkspace [M]ake session' }),
