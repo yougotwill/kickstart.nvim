a11849439ad5b8cf9241b9c555dc05db6434e6e8
William Grant <willmgrant@gmail.com>
2024-07-17 10:01:32 +1000

    added copilot plugin

--- init.lua
+++ init.lua
@@ -756,7 +756,7 @@ require('lazy').setup({
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
           --  completions whenever it has completion options available.
-          -- ['<C-Space>'] = cmp.mapping.complete {},
+          ['<C-Space>'] = cmp.mapping.complete {},
 
           -- Think of <c-l> as moving to the right of your snippet expansion.
           --  So if you have a snippet that's like:
