ae9db9aef6ab952b890e343d3c31f2b0155c07f7
yougotwill <yougotwill94@gmail.com>
2025-02-03 20:27:20 +1100

    set indentation to spaces instead of tabs

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+-- indentation
+-- 2 spaces instead of tab
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
 return {}
