4e7f28e36bbf07aae00f0a713422770bcc9c8ed2
yougotwill <yougotwill94@gmail.com>
2025-03-01 12:46:14 +1100

    added support for monokai pro theme in neovim

--- init.lua
+++ init.lua
@@ -861,6 +861,7 @@ require('lazy').setup({
     end,
   },
 
+    -- Arctic colorscheme
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
@@ -875,11 +876,73 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'arctic'
-
+  
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
   },
+  -- Monokai Pro colorscheme
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --     'loctvl842/monokai-pro.nvim',
+  --     priority = 1000, -- Make sure to load this before all the other start plugins.
+  --     config = function()
+  --       require('monokai-pro').setup {
+  --         styles = {
+  --           comment = { italic = false },
+  --           keyword = { italic = false }, -- any other keyword
+  --           type = { italic = false }, -- (preferred) int, long, char, etc
+  --           storageclass = { italic = false }, -- static, register, volatile, etc
+  --           structure = { italic = false }, -- struct, union, enum, etc
+  --           parameter = { italic = false }, -- parameter pass in function
+  --           annotation = { italic = false },
+  --           tag_attribute = { italic = false }, -- attribute of tag in reactjs
+  --         },
+  --         filter = 'classic', -- classic | octagon | pro | machine | ristretto | spectrum | light
+  --         -- Enable this will disable filter option
+  --         day_night = {
+  --           enable = false, -- turn off by default
+  --           day_filter = 'light', -- classic | octagon | pro | machine | ristretto | spectrum | light
+  --           night_filter = 'classic', -- classic | octagon | pro | machine | ristretto | spectrum | light
+  --         },
+  --         inc_search = 'background', -- underline | background
+  --         background_clear = {
+  --           -- "float_win",
+  --           'toggleterm',
+  --           'telescope',
+  --           -- "which-key",
+  --           'renamer',
+  --           'notify',
+  --           -- "nvim-tree",
+  --           -- "neo-tree",
+  --           -- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
+  --         }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
+  --         plugins = {
+  --           bufferline = {
+  --             underline_selected = false,
+  --             underline_visible = false,
+  --           },
+  --           indent_blankline = {
+  --             context_highlight = 'default', -- default | pro
+  --             context_start_underline = false,
+  --           },
+  --         },
+  --         override = function()
+  --           return {}
+  --         end,
+  --       }
+  --     --
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'monokai-pro'
+  --     -- You can configure highlights by doing something like:
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
