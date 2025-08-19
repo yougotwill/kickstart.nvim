cb7d8186d1b1dab9da9d80881a6b0490e6fc2cdf
yougotwill <yougotwill94@gmail.com>
2025-08-19 11:22:38 +0200

    feat: customisations
    - indentation default is 2 spaces
    - barbar for ui tabs
    - custom keymaps for buffers, workspace sessions, telescope
    - nerd font is on
    - no delay for whichkey
    - cpp is not disabled
    - eslintd and prettierd for js, ts, jsx, tsx by default
    - vs-code colorscheme (rockzhang24/arctic.nvim)
    - enabled indent_line, autopairs and neo-tree kickstart plugins

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -304,7 +304,7 @@ require('lazy').setup({
     opts = {
       -- delay between pressing a key and opening which-key (milliseconds)
       -- this setting is independent of vim.o.timeoutlen
-      delay = 0,
+      -- delay = 0,
       icons = {
         -- set icon mappings to true if you have a Nerd Font
         mappings = vim.g.have_nerd_font,
@@ -345,6 +345,7 @@ require('lazy').setup({
       -- Document existing key chains
       spec = {
         { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       },
@@ -412,6 +413,7 @@ require('lazy').setup({
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
+        defaults = require('telescope.themes').get_ivy {},
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
@@ -671,9 +673,19 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {
+          -- cmd = {...},
+          -- filetypes = {...},
+          -- capabilities = {...},
+          -- setings = {...},
+        },
         -- gopls = {},
-        -- pyright = {},
+        pyright = {
+          -- cmd = {...},
+          -- filetypes = {...},
+          -- capabilities = {...},
+          -- setings = {...},
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -681,7 +693,12 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {
+          -- cmd = {...},
+          -- filetypes = {...},
+          -- capabilities = {...},
+          -- setings = {...},
+        },
         --
 
         lua_ls = {
@@ -756,7 +773,10 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = {
+          c = true,
+          -- cpp = true
+        }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
@@ -773,6 +793,10 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'eslintd', 'prettierd', 'prettier' },
+        javascriptreact = { 'eslintd', 'prettierd', 'prettier' },
+        typescript = { 'eslintd', 'prettierd', 'prettier' },
+        typescriptreact = { 'eslintd', 'prettierd', 'prettier' },
       },
     },
   },
@@ -876,27 +900,95 @@ require('lazy').setup({
     },
   },
 
+  -- Arctic colorscheme
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'rockyzhang24/arctic.nvim',
+    branch = 'v2',
+    dependencies = { 'rktjmp/lush.nvim' },
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
+      -- ---@diagnostic disable-next-line: missing-fields
+      -- require('tokyonight').setup {
+      --   styles = {
+      --     comments = { italic = false }, -- Disable italics in comments
+      --   },
+      -- }
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'arctic'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
     end,
   },
+  -- Monokai Pro colorscheme
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --     'loctvl842/monokai-pro.nvim',
+  --     priority = 1000, -- Make sure to load this before all the other start plugins.
+  --     config = function()
+  --       require('monokai-pro').setup {
+  --         styles = {
+  --           comment = { italic = false },
+  --           keyword = { italic = false }, -- any other keyword
+  --           type = { italic = false }, -- (preferred) int, long, char, etc
+  --           storageclass = { italic = false }, -- static, register, volatile, etc
+  --           structure = { italic = false }, -- struct, union, enum, etc
+  --           parameter = { italic = false }, -- parameter pass in function
+  --           annotation = { italic = false },
+  --           tag_attribute = { italic = false }, -- attribute of tag in reactjs
+  --         },
+  --         filter = 'classic', -- classic | octagon | pro | machine | ristretto | spectrum | light
+  --         -- Enable this will disable filter option
+  --         day_night = {
+  --           enable = false, -- turn off by default
+  --           day_filter = 'light', -- classic | octagon | pro | machine | ristretto | spectrum | light
+  --           night_filter = 'classic', -- classic | octagon | pro | machine | ristretto | spectrum | light
+  --         },
+  --         inc_search = 'background', -- underline | background
+  --         background_clear = {
+  --           -- "float_win",
+  --           'toggleterm',
+  --           'telescope',
+  --           -- "which-key",
+  --           'renamer',
+  --           'notify',
+  --           -- "nvim-tree",
+  --           -- "neo-tree",
+  --           -- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
+  --         }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
+  --         plugins = {
+  --           bufferline = {
+  --             underline_selected = false,
+  --             underline_visible = false,
+  --           },
+  --           indent_blankline = {
+  --             context_highlight = 'default', -- default | pro
+  --             context_start_underline = false,
+  --           },
+  --         },
+  --         override = function()
+  --           return {}
+  --         end,
+  --       }
+  --     --
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'monokai-pro'
+  --     -- You can configure highlights by doing something like:
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
@@ -974,17 +1066,17 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
