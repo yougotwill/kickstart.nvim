7e3d0a652402efa8e20481e90fc60ca2bcee4ef6
yougotwill <yougotwill94@gmail.com>
2025-08-26 08:56:24 +0200

    feat: minimal setup
    Lightweight performance branch. No lsp, formatters or snippers. Requires
    git and gcc to be installed.

--- init.lua
+++ init.lua
@@ -675,50 +675,50 @@ require('lazy').setup({
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-      local servers = {
-        clangd = {
-          -- cmd = {...},
-          -- filetypes = {...},
-          -- capabilities = {...},
-          -- setings = {...},
-        },
-        -- gopls = {},
-        pyright = {
-          -- cmd = {...},
-          -- filetypes = {...},
-          -- capabilities = {...},
-          -- setings = {...},
-        },
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {
-          -- cmd = {...},
-          -- filetypes = {...},
-          -- capabilities = {...},
-          -- setings = {...},
-        },
-        --
-
-        lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
-            },
-          },
-        },
-      }
+      -- local servers = {
+      --   clangd = {
+      --     -- cmd = {...},
+      --     -- filetypes = {...},
+      --     -- capabilities = {...},
+      --     -- setings = {...},
+      --   },
+      --   -- gopls = {},
+      --   pyright = {
+      --     -- cmd = {...},
+      --     -- filetypes = {...},
+      --     -- capabilities = {...},
+      --     -- setings = {...},
+      --   },
+      --   -- rust_analyzer = {},
+      --   -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+      --   --
+      --   -- Some languages (like typescript) have entire language plugins that can be useful:
+      --   --    https://github.com/pmizio/typescript-tools.nvim
+      --   --
+      --   -- But for many setups, the LSP (`ts_ls`) will work just fine
+      --   ts_ls = {
+      --     -- cmd = {...},
+      --     -- filetypes = {...},
+      --     -- capabilities = {...},
+      --     -- setings = {...},
+      --   },
+      --   --
+      --
+      --   lua_ls = {
+      --     -- cmd = { ... },
+      --     -- filetypes = { ... },
+      --     -- capabilities = {},
+      --     settings = {
+      --       Lua = {
+      --         completion = {
+      --           callSnippet = 'Replace',
+      --         },
+      --         -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+      --         -- diagnostics = { disable = { 'missing-fields' } },
+      --       },
+      --     },
+      --   },
+      -- }
 
       -- Ensure the servers and tools above are installed
       --
@@ -735,7 +735,7 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        -- 'stylua', -- Used to format Lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -790,16 +790,16 @@ require('lazy').setup({
         end
       end,
       formatters_by_ft = {
-        lua = { 'stylua' },
+        -- lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
-        javascript = { 'eslintd', 'prettierd', 'prettier' },
-        javascriptreact = { 'eslintd', 'prettierd', 'prettier' },
-        typescript = { 'eslintd', 'prettierd', 'prettier' },
-        typescriptreact = { 'eslintd', 'prettierd', 'prettier' },
+        -- javascript = { 'eslintd', 'prettierd', 'prettier' },
+        -- javascriptreact = { 'eslintd', 'prettierd', 'prettier' },
+        -- typescript = { 'eslintd', 'prettierd', 'prettier' },
+        -- typescriptreact = { 'eslintd', 'prettierd', 'prettier' },
       },
     },
   },
@@ -810,31 +810,31 @@ require('lazy').setup({
     version = '1.*',
     dependencies = {
       -- Snippet Engine
-      {
-        'L3MON4D3/LuaSnip',
-        version = '2.*',
-        build = (function()
-          -- Build Step is needed for regex support in snippets.
-          -- This step is not supported in many windows environments.
-          -- Remove the below condition to re-enable on windows.
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
-        },
-        opts = {},
-      },
+      -- {
+      --   'L3MON4D3/LuaSnip',
+      --   version = '2.*',
+      --   build = (function()
+      --     -- Build Step is needed for regex support in snippets.
+      --     -- This step is not supported in many windows environments.
+      --     -- Remove the below condition to re-enable on windows.
+      --     if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+      --       return
+      --     end
+      --     return 'make install_jsregexp'
+      --   end)(),
+      --   dependencies = {
+      --     -- `friendly-snippets` contains a variety of premade snippets.
+      --     --    See the README about individual language/framework/plugin snippets:
+      --     --    https://github.com/rafamadriz/friendly-snippets
+      --     -- {
+      --     --   'rafamadriz/friendly-snippets',
+      --     --   config = function()
+      --     --     require('luasnip.loaders.from_vscode').lazy_load()
+      --     --   end,
+      --     -- },
+      --   },
+      --   opts = {},
+      -- },
       'folke/lazydev.nvim',
     },
     --- @module 'blink.cmp'
@@ -887,7 +887,7 @@ require('lazy').setup({
         },
       },
 
-      snippets = { preset = 'luasnip' },
+      -- snippets = { preset = 'luasnip' },
 
       -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
       -- which automatically downloads a prebuilt binary when enabled.
