b944b23cb26ce2d10f0d7503b73c11d7602e6110
yougotwill <yougotwill94@gmail.com>
2026-03-24 10:13:09 +0200

    feat: when a terminal is opened enter into insert mode automatically

--- init.lua
+++ init.lua
@@ -222,6 +222,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Enter insert mode when focusing a terminal buffer
+vim.api.nvim_create_autocmd({ 'BufEnter', 'WinEnter', 'TermOpen' }, {
+  pattern = 'term://*',
+  command = 'startinsert',
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
