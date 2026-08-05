5166eddc6953b79c8375950540a5666754ce7bb2
yougotwill <yougotwill94@gmail.com>
2026-08-05 10:26:37 +1000

    feat: disabled hardtime and updated deps

--- lua/custom/plugins/hardtime.lua
+++ lua/custom/plugins/hardtime.lua
@@ -1,10 +1,11 @@
 return {
-  {
-    'm4xshen/hardtime.nvim',
-    dependencies = { 'MunifTanjim/nui.nvim' },
-    init = function() end,
-    opts = {
-      disable_mouse = false,
-    },
-  },
+  -- {
+  --   'm4xshen/hardtime.nvim',
+  --   dependencies = { 'MunifTanjim/nui.nvim' },
+  --   init = function() end,
+  --   opts = {
+  --     max_count = 7,
+  --     disable_mouse = false,
+  --   },
+  -- },
 }
